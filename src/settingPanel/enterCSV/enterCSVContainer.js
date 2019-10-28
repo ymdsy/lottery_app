eb3f4eccfb1c598424a5fea604457b5f8dab6b11
ymdsy <yamada@example.com>
2019-10-28 11:35:19 +0900

    くじ入力画面にスロットルを実装

--- src/settingPanel/enterCSV/enterCSVContainer.js
+++ src/settingPanel/enterCSV/enterCSVContainer.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { EnterCSVPresenter } from "./enterCSVPresenter.js";
+import throttle from "lodash/throttle";
 
 export class EnterCSVContainer extends React.Component {
   constructor(props) {
     super(props);
-    this.enterCsv = this.enterCsv.bind(this);
+    this.enterCsv = throttle(this.enterCsv.bind(this), 1500);
   }
 
-  enterCsv(event) {
-    const parsedData = event.target.value
+  enterCsv(value) {
+    const parsedData = value
       .split(/[ ]{0,},{1,}/)
       .filter(value => value !== " " && value.length > 0);
     this.props.updateParsedData(parsedData);
@@ -19,7 +20,7 @@ export class EnterCSVContainer extends React.Component {
       <>
         EnterCSVContainer
         <br />
-        <EnterCSVPresenter enterCsv={data => this.enterCsv(data)} />
+        <EnterCSVPresenter enterCsv={value => this.enterCsv(value)} />
       </>
     );
   }
--- src/settingPanel/enterCSV/enterCSVPresenter.js
+++ src/settingPanel/enterCSV/enterCSVPresenter.js
@@ -3,7 +3,7 @@ import React from "react";
 export function EnterCSVPresenter(props) {
   return (
     <>
-      <textarea onChange={props.enterCsv} />
+      <textarea onChange={event => props.enterCsv(event.target.value)} />
     </>
   );
 }
